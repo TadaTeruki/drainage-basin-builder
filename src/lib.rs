590eabe0d861f70f005e848826fe30e40032eba9
Teruki Tada <me@peruki.dev>
2025-01-25 12:02:44 +0900

    initial

---
+++ src/lib.rs
@@ -0,0 +1,100 @@
+use std::{collections::HashMap, i32};
+
+use worley_particle::{map::ParticleMap, Particle};
+
+#[derive(Debug, Clone, PartialEq)]
+struct Node {
+    area: f64,
+    flow_to: Particle,
+}
+
+pub fn build_drainage_area(terrain_map: &ParticleMap<f64>) -> ParticleMap<f64> {
+    let node = terrain_map
+        .iter()
+        .map(|(&particle, elevation)| {
+            let voronoi = particle.calculate_voronoi();
+            let area = voronoi.area();
+            let mut flow_to = None;
+            let mut steepest_slope = 0.0;
+            let site = particle.site();
+            for neighbor in voronoi.neighbors {
+                if let Some(neighbor_elevation) = terrain_map.get(&neighbor) {
+                    if neighbor_elevation > elevation {
+                        continue;
+                    }
+                    let neighbor_site = neighbor.site();
+                    let distance = (site.0 - neighbor_site.0).hypot(site.1 - neighbor_site.1);
+                    let slope = (elevation - neighbor_elevation) / distance;
+                    if let Some(_) = flow_to {
+                        if slope > steepest_slope {
+                            steepest_slope = slope;
+                            flow_to = Some(neighbor);
+                        }
+                    } else {
+                        steepest_slope = slope;
+                        flow_to = Some(neighbor);
+                    }
+                }
+            }
+            if let Some(flow_to) = flow_to {
+                (particle, Node { area, flow_to })
+            } else {
+                (
+                    particle,
+                    Node {
+                        area,
+                        flow_to: particle,
+                    },
+                )
+            }
+        })
+        .collect::<ParticleMap<Node>>();
+
+    let mut parent_num = HashMap::new();
+
+    for (_, node) in node.iter() {
+        let flow_to = node.flow_to;
+        if !parent_num.contains_key(&flow_to) {
+            parent_num.insert(flow_to, 1);
+        } else {
+            *parent_num.get_mut(&flow_to).unwrap() += 1;
+        }
+    }
+
+    let mut drainage_area = HashMap::new();
+
+    for (particle, node) in node.iter() {
+        if parent_num.contains_key(&particle) {
+            continue;
+        }
+        let mut particle = *particle;
+        loop {
+            drainage_area
+                .entry(particle)
+                .and_modify(|e| {
+                    *e += node.area;
+                })
+                .or_insert(node.area);
+
+            if node.flow_to == particle {
+                break;
+            }
+
+            drainage_area
+                .entry(node.flow_to)
+                .and_modify(|e| {
+                    *e += node.area;
+                })
+                .or_insert(node.area);
+
+            particle = node.flow_to;
+
+            if *parent_num.get(&particle).unwrap_or(&i32::MAX) > 1 {
+                parent_num.entry(particle).and_modify(|e| *e -= 1);
+                break;
+            }
+        }
+    }
+
+    ParticleMap::new(*terrain_map.params(), drainage_area)
+}
