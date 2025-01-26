f609d6d6a4a491734fe60142bd5e3d79486d152b
Teruki Tada <me@peruki.dev>
2025-01-26 14:58:27 +0900

    work

--- examples/terrain.rs
+++ examples/terrain.rs
@@ -1,6 +1,6 @@
 use std::{cell::RefCell, rc::Rc};
 
-use drainage_basin_builder::{build_drainage_basin, DrainageBasinInput, DrainageBasinNode};
+use drainage_basin_builder::map::DrainageMap;
 use gtk4::{cairo::Context, prelude::WidgetExt, DrawingArea};
 use vislayers::{
     colormap::SimpleColorMap,
@@ -67,8 +67,16 @@ impl Layer for TerrainMap {
             cr.new_path();
             for polygon in &threshold.polygons {
                 for (i, point) in polygon.iter().enumerate().step_by(bands_step) {
-                    let x = rect.map_coord_x(point.0, 0.0, area_width as f64);
-                    let y = rect.map_coord_y(point.1, 0.0, area_height as f64);
+                    let x = rect.map_coord_x(
+                        point.0 - self.particle_map.params().get_scale() / 2.0,
+                        0.0,
+                        area_width as f64,
+                    );
+                    let y = rect.map_coord_y(
+                        point.1 - self.particle_map.params().get_scale() / 2.0,
+                        0.0,
+                        area_height as f64,
+                    );
 
                     if i == 0 {
                         cr.move_to(x, y);
@@ -96,80 +104,64 @@ impl Layer for TerrainMap {
     }
 }
 
-struct DrainageMap {
-    particle_map: ParticleMap<DrainageBasinNode>,
-}
+struct DrainageMapWrapped(DrainageMap);
 
-impl DrainageMap {
-    fn from_terrain_map(terrain_map: &TerrainMap) -> Self {
-        let particle_map_input = terrain_map
-            .particle_map
-            .iter()
-            .map(|(particle, elevation)| {
-                (
-                    *particle,
-                    DrainageBasinInput {
-                        elevation: *elevation,
-                    },
-                )
-            })
-            .collect::<ParticleMap<DrainageBasinInput>>();
-
-        let particle_map = build_drainage_basin(&particle_map_input);
-
-        Self { particle_map }
-    }
-
-    fn save(&self, file_path: &str) {
-        self.particle_map
-            .write_to_file(file_path)
-            .expect("Error writing drainage map");
-    }
-
-    fn load(file_path: &str) -> Self {
-        let particle_map = ParticleMap::<DrainageBasinNode>::read_from_file(file_path)
-            .expect("Error reading drainage map");
-
-        Self { particle_map }
-    }
-}
-
-impl Layer for DrainageMap {
+impl Layer for DrainageMapWrapped {
     fn draw(&self, drawing_area: &DrawingArea, cr: &Context, focus_range: &FocusRange) {
         let area_width = drawing_area.width();
         let area_height = drawing_area.height();
 
         let rect = focus_range.to_rect(area_width as f64, area_height as f64);
 
-        for (particle, node) in self.particle_map.iter() {
-            let site_from = particle.site();
-            let site_to = node.flow_to.site();
+        if focus_range.radius() > 0.1 {
+            for (_, node) in self.0.particle_map().iter() {
+                let river_width = node.river_width(self.0.river_strength());
+                if river_width < self.0.river_ignoreable_width() {
+                    continue;
+                }
+                let iter_num = (0.1 / focus_range.radius()).ceil() as usize;
 
-            let x1 = rect.map_coord_x(site_from.0, 0.0, area_width as f64);
-            let y1 = rect.map_coord_y(site_from.1, 0.0, area_height as f64);
-            let x2 = rect.map_coord_x(site_to.0, 0.0, area_width as f64);
-            let y2 = rect.map_coord_y(site_to.1, 0.0, area_height as f64);
+                let point_0 = node.main_river.evaluate(0.0);
+                let x0 = rect.map_coord_x(point_0.0, 0.0, area_width as f64);
+                let y0 = rect.map_coord_y(point_0.1, 0.0, area_height as f64);
 
-            if *particle == node.flow_to {
-                cr.set_source_rgb(1.0, 0.0, 1.0);
-                cr.arc(x1, y1, 1.0, 0.0, 2.0 * std::f64::consts::PI);
-                cr.fill().expect("Failed to draw center point");
-            } else {
-                let site_second_to = self
-                    .particle_map
-                    .get(&node.flow_to)
-                    .unwrap_or(&node)
-                    .flow_to
-                    .site();
-                let x3 = rect.map_coord_x(site_second_to.0, 0.0, area_width as f64);
-                let y3 = rect.map_coord_y(site_second_to.1, 0.0, area_height as f64);
+                cr.move_to(x0, y0);
 
-                cr.set_line_width(node.river_width(1.0) / focus_range.radius());
+                for i in 1..(iter_num + 1) {
+                    let t = i as f64 / iter_num as f64;
+
+                    let point_1 = node.main_river.evaluate(t);
+                    let x1 = rect.map_coord_x(point_1.0, 0.0, area_width as f64);
+                    let y1 = rect.map_coord_y(point_1.1, 0.0, area_height as f64);
+
+                    cr.line_to(x1, y1);
+                }
+
+                cr.set_line_width(
+                    river_width / focus_range.radius() / self.0.particle_map().params().get_scale(),
+                );
                 cr.set_source_rgb(0.0, 0.0, 1.0);
-                cr.move_to((x1 + x2) / 2.0, (y1 + y2) / 2.0);
-                cr.line_to((x2 + x3) / 2.0, (y2 + y3) / 2.0);
                 cr.set_line_cap(gtk4::cairo::LineCap::Round);
                 cr.stroke().expect("Failed to draw edge");
+            }
+        } else {
+            let img_width = drawing_area.width();
+            let img_height = drawing_area.height();
+
+            for iy in (0..img_height).step_by(6) {
+                for ix in (0..img_width).step_by(6) {
+                    let prop_x = (ix as f64) / img_width as f64;
+                    let prop_y = (iy as f64) / img_height as f64;
+
+                    let x = rect.min_x + prop_x * rect.width();
+                    let y = rect.min_y + prop_y * rect.height();
+
+                    if self.0.collides_with_river(x, y) {
+                        cr.set_source_rgb(0.0, 0.0, 1.0);
+                        cr.rectangle(ix as f64 - 1.0, iy as f64 - 1.0, 3.0, 3.0);
+                        cr.fill().expect("Failed to fill rectangle");
+                    }
+                }
             }
         }
     }
@@ -177,13 +169,13 @@ impl Layer for DrainageMap {
 
 fn main() {
     let terrain_map = TerrainMap::new("./data/in/11008264925851530191.particlemap", 0.0025);
-    let drainage_map = DrainageMap::from_terrain_map(&terrain_map);
-    drainage_map.save("./data/out/drainage-11008264925851530191.particlemap");
+    let drainage_map = DrainageMap::from_elevation_map(&terrain_map.particle_map, 1.0, 0.01);
+    drainage_map.save_to_file("./data/out/drainage.particlemap");
 
-    let drainage_map = DrainageMap::load("./data/out/drainage-11008264925851530191.particlemap");
+    let drainage_map = DrainageMap::load_from_file("./data/out/drainage.particlemap", 1.0, 0.01);
 
     let mut visualizer = Visualizer::new(800, 600);
     visualizer.add_layer(Rc::new(RefCell::new(terrain_map)), 0);
-    visualizer.add_layer(Rc::new(RefCell::new(drainage_map)), 1);
+    visualizer.add_layer(Rc::new(RefCell::new(DrainageMapWrapped(drainage_map))), 1);
     visualizer.run();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,13 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, vec};
 
+use bezier_rs::{Bezier, BezierHandles, TValue};
+use glam::DVec2;
 use worley_particle::{
     map::{ParticleMap, ParticleMapAttributeRW},
     Particle,
 };
+
+pub mod map;
 
 #[derive(Debug, Clone, PartialEq)]
 struct InternalNode {
@@ -18,21 +22,136 @@ pub struct DrainageBasinInput {
 }
 
 #[derive(Debug, Clone, PartialEq)]
+pub enum Stream {
+    Path(Bezier),
+    Point((f64, f64)),
+}
+
+impl ParticleMapAttributeRW for Stream {
+    fn from_strs(s: &[&str]) -> Result<Self, Box<dyn std::error::Error>> {
+        if s[0] == "Path" {
+            let start = DVec2 {
+                x: s[1].parse::<f64>()?,
+                y: s[2].parse::<f64>()?,
+            };
+            let handle = DVec2 {
+                x: s[3].parse::<f64>()?,
+                y: s[4].parse::<f64>()?,
+            };
+            let end = DVec2 {
+                x: s[5].parse::<f64>()?,
+                y: s[6].parse::<f64>()?,
+            };
+            Ok(Stream::Path(Bezier::from_quadratic_coordinates(
+                start.x, start.y, handle.x, handle.y, end.x, end.y,
+            )))
+        } else {
+            let x = s[1].parse::<f64>()?;
+            let y = s[2].parse::<f64>()?;
+            Ok(Stream::Point((x, y)))
+        }
+    }
+
+    fn to_strings(&self) -> Vec<String> {
+        match self {
+            Stream::Path(path) => {
+                let handle = match path.handles {
+                    BezierHandles::Quadratic { handle } => handle,
+                    _ => unreachable!(),
+                };
+                vec![
+                    "Path".to_string(),
+                    path.start.x.to_string(),
+                    path.start.y.to_string(),
+                    handle.x.to_string(),
+                    handle.y.to_string(),
+                    path.end.x.to_string(),
+                    path.end.y.to_string(),
+                ]
+            }
+            Stream::Point((x, y)) => vec![
+                "Point".to_string(),
+                x.to_string(),
+                y.to_string(),
+                "".to_string(),
+                "".to_string(),
+                "".to_string(),
+                "".to_string(),
+            ],
+        }
+    }
+
+    fn len_strs() -> usize {
+        7
+    }
+}
+
+impl Stream {
+    pub fn new(site_0: (f64, f64), site_1: (f64, f64), site_2: (f64, f64)) -> Self {
+        let path_start = ((site_0.0 + site_1.0) / 2.0, (site_0.1 + site_1.1) / 2.0);
+        if site_0 == site_1 {
+            return Stream::Point(site_0);
+        }
+        let path_end = ((site_1.0 + site_2.0) / 2.0, (site_1.1 + site_2.1) / 2.0);
+        let path = Bezier::from_quadratic_coordinates(
+            path_start.0,
+            path_start.1,
+            site_1.0,
+            site_1.1,
+            path_end.0,
+            path_end.1,
+        );
+
+        Stream::Path(path)
+    }
+
+    pub fn evaluate(&self, t: f64) -> (f64, f64) {
+        match self {
+            Stream::Path(path) => {
+                let point = path.evaluate(TValue::Parametric(t));
+                (point.x, point.y)
+            }
+            Stream::Point((x, y)) => (*x, *y),
+        }
+    }
+
+    pub fn collides(&self, x: f64, y: f64, width: f64) -> bool {
+        match self {
+            Stream::Path(path) => {
+                let projection = path.project(DVec2 { x, y }, None);
+                let projection_point = path.evaluate(TValue::Parametric(projection));
+                let squared_distance =
+                    (projection_point.x - x).powi(2) + (projection_point.y - y).powi(2);
+                squared_distance < width.powi(2)
+            }
+            Stream::Point((x0, y0)) => {
+                let squared_distance = (x0 - x).powi(2) + (y0 - y).powi(2);
+                squared_distance < width.powi(2)
+            }
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
 pub struct DrainageBasinNode {
     pub particle: Particle,
     pub area: f64,
     pub drainage_area: f64,
     pub slope: f64,
     pub flow_to: Particle,
+    pub main_river: Stream,
 }
 
 impl ParticleMapAttributeRW for DrainageBasinNode {
     fn from_strs(s: &[&str]) -> Result<Self, Box<dyn std::error::Error>> {
         let particle = Particle::from_strs(&s[..Particle::len_strs()])?;
         let flow_to = Particle::from_strs(&s[Particle::len_strs()..Particle::len_strs() * 2])?;
-        let area = s[Particle::len_strs() * 2].parse()?;
-        let drainage_area = s[Particle::len_strs() * 2 + 1].parse()?;
-        let slope = s[Particle::len_strs() * 2 + 2].parse()?;
+        let main_river = Stream::from_strs(
+            &s[Particle::len_strs() * 2..Particle::len_strs() * 2 + Stream::len_strs()],
+        )?;
+        let area = s[Particle::len_strs() * 2 + Stream::len_strs()].parse::<f64>()?;
+        let drainage_area = s[Particle::len_strs() * 2 + Stream::len_strs() + 1].parse::<f64>()?;
+        let slope = s[Particle::len_strs() * 2 + Stream::len_strs() + 2].parse::<f64>()?;
 
         Ok(DrainageBasinNode {
             particle,
@@ -40,23 +159,30 @@ impl ParticleMapAttributeRW for DrainageBasinNode {
             drainage_area,
             slope,
             flow_to,
+            main_river,
         })
     }
 
     fn to_strings(&self) -> Vec<String> {
         let particle = self.particle.to_strings();
         let flow_to = self.flow_to.to_strings();
+        let main_river = self.main_river.to_strings();
         let others = vec![
             self.area.to_string(),
             self.drainage_area.to_string(),
             self.slope.to_string(),
         ];
 
-        particle.into_iter().chain(flow_to).chain(others).collect()
+        particle
+            .into_iter()
+            .chain(flow_to)
+            .chain(main_river)
+            .chain(others)
+            .collect()
     }
 
     fn len_strs() -> usize {
-        Particle::len_strs() + Particle::len_strs() + 3
+        Particle::len_strs() + Particle::len_strs() + Stream::len_strs() + 3
     }
 }
 
@@ -68,7 +194,7 @@ impl DrainageBasinNode {
     }
 
     pub fn river_width(&self, strength: f64) -> f64 {
-        self.drainage_area.sqrt() * strength
+        self.drainage_area.sqrt() * strength * self.particle.params().get_scale()
     }
 }
 
@@ -167,12 +293,23 @@ pub fn build_drainage_basin(
 
             if *parent_num.get(&flow_to).unwrap() > 1 {
                 parent_num.entry(flow_to).and_modify(|e| *e -= 1);
-
                 break;
             }
 
             current = flow_to;
         }
+    }
+
+    let mut river_paths = HashMap::new();
+
+    for (particle, node) in nodes.iter() {
+        let flow_to = node.flow_to;
+        if flow_to == *particle {
+            continue;
+        }
+        let second_flow_to = nodes.get(&flow_to).unwrap().flow_to;
+        let (site_0, site_1, site_2) = (particle.site(), flow_to.site(), second_flow_to.site());
+        river_paths.insert(*particle, Stream::new(site_0, site_1, site_2));
     }
 
     nodes
@@ -186,6 +323,7 @@ pub fn build_drainage_basin(
                     drainage_area: *drainage_area.get(particle)?,
                     flow_to: node.flow_to,
                     slope: node.slope,
+                    main_river: river_paths.get(particle)?.clone(),
                 },
             ))
         })
